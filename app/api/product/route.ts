bc8b077d009a61553715099a0278eebc4e867b8c
Israel Ojehonmon <ojehonmonisrael008@gmail.com>
2025-11-16 09:02:28 -0800

    jjd

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 import productsData from '@/data/products.json';
 
-
-export default function handler() {
-              return NextResponse.json(productsData);
+export async function GET() {
+  return NextResponse.json(productsData);
 }
